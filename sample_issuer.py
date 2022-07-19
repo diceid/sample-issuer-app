95ed910f90761fffc2187d47b91e971bc7f05687
prachi.bhatkar@wipro.com <prachi.bhatkar@wipro.com>
2022-07-19 05:46:56 +0000

    modified sample_issuer

--- sample_issuer.py
+++ sample_issuer.py
@@ -8,13 +8,34 @@ from aiohttp import (
     ClientError,
     ClientTimeout,
 )
+import time
+import os
 
+async def check_existing_connection_id(issuer_url, auth_code):
+    conn_id = input ("Enter connection id: ")
+    path="/api/connection/"
+    print("Getting connection infromation...\n")
+    headers = {
+                'Authorization': 'Bearer ' + auth_code
+               }
+
+    print ("requesr url = ", issuer_url  + path + conn_id)
+    async with ClientSession() as session:
+        async with session.request("GET", issuer_url + path + conn_id, headers=headers) as resp:
+            if resp.status != 200:
+                raise Exception(
+                    f"Error response code {resp.status}"
+                )
+            conn_resp=await resp.json()
+            state=conn_resp["state"]
+            print(f"Connection id= {conn_id}, Connection state= {state}\n")
+    return conn_id
 
 async def print_oob_connection_invitation(issuer_url, auth_code):
     conn_id=None
-    print("Creating connection invitation...\n")
     path="/submit"
     emailId=input("Enter your emailId:")
+    print("Creating connection invitation...\n")
     params = {
     			"emailId": emailId
     	    }
@@ -31,10 +52,32 @@ async def print_oob_connection_invitation(issuer_url, auth_code):
             idx=conn_info.find("id=")+4
             conn_id=conn_info[idx:idx+36]
             
-            print(f"Connection Invitation Created:{conn_id} !!!!!!\n")
-            print("::::::Go to given mail id for QR Code:::::\n")
+            print(f"Connection Invitation Created: {conn_id} !!!!!!\n")
+            print("Email is sent with invitation link, please click on the link and scan the QR code with DICE wallet\n")
 
     return conn_id     
+
+
+async def use_existing_schema(issuer_url, auth_code):
+    schema_id= input ("Enter schema id: ")
+    seqno=None
+    headers = {
+                'Authorization': 'Bearer ' + auth_code
+            }
+    print("Fetching schema information...\n")
+    path="/api/schema/"
+
+    async with ClientSession() as session:
+        async with session.request("GET", issuer_url + path + schema_id, headers=headers) as resp:
+            if resp.status != 200:
+                raise Exception(
+                    f"Error response code {resp.status}"
+                )
+            conn_resp=await resp.json()
+            seqno=conn_resp["seq_num"]
+            print(f"Schema id= {schema_id}, Schema SeqNo= {seqno}\n")
+    return schema_id,seqno
+        
 
 
 async def create_schema(issuer_url, auth_code):
@@ -46,10 +89,9 @@ async def create_schema(issuer_url, auth_code):
     			
                 "attributes": {
                                 "Name":"text",
-                                "Email":"text",
-                                "SPOC_Email":"text",
-                                "Credential_Valid_From":"text",
-                                "Credential_Valid_Till":"text"
+                                "Age":"text",
+                                "PAN":"text",
+                                "PostalAddress":"text"
                             },
                 "schema_name": "name" + str(randint(0,100)),
                 "schema_version": "1.0"                                
@@ -86,6 +128,13 @@ async def send_credential(issuer_url, auth_code, conn_id, schema):
     contents=schema[0].split(":")
     cred_def_id=contents[0]+":3:CL:"+schema[1]+":default"
     print("cred_def_id:",cred_def_id)
+    name=input("Enter Name: ")
+    age=input("Enter Age: ")
+    pan=input("Enter PAN: ")
+    pa=input("Enter Postal Address: ")
+    conn_id=input("Enter connection id: ")
+    valid_from = int(time.time())
+    valid_to = valid_from + 31536000
     payload = {
                     "auto_issue": "true",
                     "comment": "string",
@@ -96,11 +145,19 @@ async def send_credential(issuer_url, auth_code, conn_id, schema):
                         "attributes": [
                         {
                             "name": "Name",
-                            "value": "John"
+                            "value": name
                         },
                         {
-                            "name": "Email",
-                            "value": "abc@gmail.com"
+                            "name": "Age",
+                            "value": age
+                        },
+                        {
+                            "name": "PAN",
+                            "value": pan
+                        },
+                        {
+                            "name": "PostalAddress",
+                            "value": pa
                         }
                         ]
                     }
@@ -122,12 +179,19 @@ async def send_credential(issuer_url, auth_code, conn_id, schema):
 
 async def main():
     conn_id=None
-    issuer_url = "https://futurebankapi.wiprobc.com"
-    auth_code = input("Input Authorization Code: ")
+    #issuer_url = "https://futurebankapi.wiprobc.com"
+    issuer_url = "https://futureapi.wiprobc.com"
+    auth_code = os.getenv("AUTHORIZATION_CODE",None)
+    if(auth_code is None):
+        auth_code = input("Input Authorization Code: ")
     print("\n")
     options = (
-            "    (1) Create Connection\n"
-            "    (2) Create Schema\n"
+            "    Connection\n"
+            "    (1a) Create New Connection\n"
+            "    (1b) Use Existing Connection\n"
+            "    Schema\n"
+            "    (2a) Create New Schema\n"
+            "    (2b) Use Existing Schema\n"
             "    (3) Send Credential to Holder\n"
             "    (X) Exit\n"
         )
@@ -135,15 +199,20 @@ async def main():
     
     while True:
         print(options)
-        Res=input("[1 / 2 / 3 / X]\n")
+        Res=input("[1a /1b / 2a / 2b / 3 / X]\n")
         if Res is None or Res in "xX":
             break
 
-        elif Res == "1":
+        elif Res == "1a":
             conn_id = await print_oob_connection_invitation(issuer_url, auth_code)
+        elif Res == "1b":
+            conn_id = await check_existing_connection_id(issuer_url, auth_code)
         
-        elif Res == "2":
+        elif Res == "2a":
             schema = await create_schema(issuer_url, auth_code)   
+        
+        elif Res == "2b":
+            schema = await use_existing_schema(issuer_url, auth_code)   
 
         elif Res == "3" and conn_id is not None and schema is not None:
             await send_credential(issuer_url, auth_code,conn_id, schema)  
